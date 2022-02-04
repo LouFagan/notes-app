74ef24bfa6d5f415e52d4ff9196511f5aead4fa5
LouFagan <lfagan@pixelmein.ca>
2022-02-04 15:23:13 -0500

    Minor changes

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ export default function App() {
 
   function updateNote(text) {
     // Try to rearrange the most recently-modified
-    // not to be at the top
+    // to be at the top
     setNotes((oldNotes) => {
       const newArray = [];
       for (let i = 0; i < oldNotes.length; i++) {
